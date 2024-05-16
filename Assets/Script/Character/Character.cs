8f23e753b4cc32bc91d8258ca66a7273e8da0dc0
RCO8 <gurwo63@naver.com>
2024-05-16 17:02:11 +0900

    기본 구성 파일 정의

---
+++ Assets/Script/Character/Character.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    int speed;
+    int power;
+    Bullet bullet;  //오브젝트 풀링 필요
+
+    void Shooting()
+    {
+
+    }
+    void Moving()
+    {
+
+    }
+    private void Dead()
+    {
+
+    }
+}
---
+++ Assets/Script/Character/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+public class Enemy : Character
+{
+    Vector2 createPos = Vector2.Zero;   //적이 랜덤으로 생성할 위치
+    void RandomCreate()
+    {
+
+    }
+}
