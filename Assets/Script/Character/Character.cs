9a5653312d63552c03c6c2002526a295efcdf4c8
RCO8 <gurwo63@naver.com>
2024-05-16 20:08:28 +0900

    플레이어 이동 기능

--- Assets/Script/Character/Character.cs
+++ Assets/Script/Character/Character.cs
@@ -2,24 +2,36 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    int speed;
+    protected float Speed { get; set; }
     int power;
     Bullet bullet;  //오브젝트 풀링 필요
 
     protected Rigidbody2D rgb2D;
+    protected Vector2 characterMovement;
 
     protected virtual void Awake()
     {
         rgb2D = GetComponent<Rigidbody2D>();
+
+        //기본 속성값 설정
+        Speed = 3f;
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Moving(characterMovement);
     }
 
     void Shooting()
     {
 
     }
-    void Moving()
+    protected void Moving(Vector2 move)
     {
+        //상위클래스는 move값을 charcterMovement에 담기만 하고
+        //실제 이동은 하위 클래스에서.
         //이동방향 * speed;
+        characterMovement = move;
     }
     void Dead()
     {
--- Assets/Script/Character/Player.cs
+++ Assets/Script/Character/Player.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Player : Character
 {
+    PlayerController controller;
 
     protected override void Awake()
     {
         base.Awake();
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Moving;
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        PlayerMoving();
+    }
+
+    private void PlayerMoving() //플레이어만의 이동기능
+    {
+        rgb2D.velocity = characterMovement.normalized * Speed;
     }
 
     // 이 메서드들은 업그레이드 하면 일정기간동안 적용되다가 원래로 리셋된다.
@@ -17,6 +35,7 @@ public class Player : Character
     }
     void SpeedUp()
     {
-
+        //스피드업
+        Speed = 5f;
     }
 }
