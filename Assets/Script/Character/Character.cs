aca0875ef7c823dfff2160fe77bd072f42b1f6de
RCO8 <gurwo63@naver.com>
2024-05-16 19:50:41 +0900

    캐릭터 클래스 rigidbody2D

--- Assets/Script/Character/Character.cs
+++ Assets/Script/Character/Character.cs
@@ -6,15 +6,22 @@ public class Character : MonoBehaviour
     int power;
     Bullet bullet;  //오브젝트 풀링 필요
 
+    protected Rigidbody2D rgb2D;
+
+    protected virtual void Awake()
+    {
+        rgb2D = GetComponent<Rigidbody2D>();
+    }
+
     void Shooting()
     {
 
     }
     void Moving()
     {
-
+        //이동방향 * speed;
     }
-    private void Dead()
+    void Dead()
     {
 
     }
--- Assets/Script/Character/Player.cs
+++ Assets/Script/Character/Player.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Player : Character
 {
 
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // 이 메서드들은 업그레이드 하면 일정기간동안 적용되다가 원래로 리셋된다.
     void PowerUp()
     {
 
