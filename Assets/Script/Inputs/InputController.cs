a6b99bc2aa20528fb44ade119eea662d8a15cfc4
RCO8 <gurwo63@naver.com>
2024-05-17 14:16:25 +0900

    Reapply "Merge branch 'Dev_THJ' into Dev_Result"
    This reverts commit 916a917c76c3aef6b3cc7e77d3e7df1c412a75c9.

---
+++ Assets/Script/Inputs/InputController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnTargetEvent; //마우스 좌표
+
+    //입력 시스템 Invoke
+    public void CallMoveEvent(Vector2 move)
+    {
+        OnMoveEvent?.Invoke(move);
+    }
+
+    public void CallTargetEvent(Vector2 target)
+    {
+        OnTargetEvent?.Invoke(target);
+    }
+}
