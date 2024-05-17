cf4839a0868d430513442c16158e750b2a49e8c9
yuyerin <annie7373@naver.com>
2024-05-17 14:36:14 +0900

    [Add] 메인씬 UI

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class GameManager : MonoBehaviour
 
     float time;
     float score;
+
+    public Text timeTxt;
+    //public Text NowScore;
+    //public Text BestScore;
+
+    bool isPlay = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +25,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlay)
+        {
+            time += Time.deltaTime;
+            timeTxt.text = time.ToString("N2");
+        }
     }
 
     void ResultUI() //결과 UI 출력
