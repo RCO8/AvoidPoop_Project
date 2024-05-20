7f05103f6021669c396b77382074146f034c9971
RCO8 <gurwo63@naver.com>
2024-05-20 16:30:10 +0900

    2인용 조준 구현중

--- Assets/Script/Inputs/PlayerController.cs
+++ Assets/Script/Inputs/PlayerController.cs
@@ -14,10 +14,22 @@ public class PlayerController : InputController
 
     public void OnTarget(InputValue value)
     {
+        //마우스로 조작한 타겟
         Vector2 targetPos = value.Get<Vector2>();
         Vector2 worldPos = cam.ScreenToWorldPoint(targetPos);
         targetPos = (worldPos - (Vector2)transform.position).normalized;
         CallTargetEvent(targetPos);
+
+    }
+
+    public void OnTargetKey(InputValue value)   //키보드 조준
+    {
+        //2인용 컨트롤러 문제로 키보드 방향키대로 바라보게 해보기
+        // 예를 들어 방향키를 아래 오른쪽으로 누르면 그 방향대로 쏘게 하기
+        Vector2 nowPos = transform.position;
+        Vector2 targetPos = value.Get<Vector2>().normalized;
+        targetPos = nowPos + targetPos;
+        //CallTargetEvent(targetPos);
     }
 
     public void OnShoot(InputValue value)
