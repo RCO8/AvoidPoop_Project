d2673810d23f61b161e3749f1a2760e4e694330d
RCO8 <gurwo63@naver.com>
2024-05-22 11:06:41 +0900

    Select씬에서 난이도 선택 추가

--- Assets/Script/SelectBtn.cs
+++ Assets/Script/SelectBtn.cs
@@ -5,10 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class SelectBtn : MonoBehaviour
 {
-    public void SelectGame(int player)
+    public void SelectGame()
     {
-        PlayerPrefs.SetInt("Player", player);
+        //각 키 속성없이 들어가면 자동으로 설정
+        if(!PlayerPrefs.HasKey("Player"))
+            PlayerPrefs.SetInt("Player", 1);
+        if (!PlayerPrefs.HasKey("Difficalty"))
+            PlayerPrefs.SetString("Difficalty", "Easy");
 
         SceneManager.LoadScene("MainGameScene");
+    }
+
+    public void SetPlayer(int player)
+    {
+        PlayerPrefs.SetInt("Player", player);
+    }
+
+    public void SetDifficalty(string diff)
+    {
+        PlayerPrefs.SetString("Difficalty", diff);
     }
 }
