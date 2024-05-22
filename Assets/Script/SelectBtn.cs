0b8f6f1cf24017e88622fecae27f0b74dfd1fafb
RCO8 <gurwo63@naver.com>
2024-05-22 11:22:10 +0900

    게임시작 자동설정 제거

--- Assets/Script/SelectBtn.cs
+++ Assets/Script/SelectBtn.cs
@@ -7,12 +7,6 @@ public class SelectBtn : MonoBehaviour
 {
     public void SelectGame()
     {
-        //각 키 속성없이 들어가면 자동으로 설정
-        if(!PlayerPrefs.HasKey("Player"))
-            PlayerPrefs.SetInt("Player", 1);
-        if (!PlayerPrefs.HasKey("Difficalty"))
-            PlayerPrefs.SetString("Difficalty", "Easy");
-
         SceneManager.LoadScene("MainGameScene");
     }
 
